92b8c1dfc6f732b6432b8f9f9705cf494900969b
pepars-Rashid <peparsrashid@gmail.com>
2024-11-15 18:36:36 -0800

    intionalization for resources page

---
+++ src/app/Student-dashboard/resources/page.jsx
@@ -0,0 +1,11 @@
+
+import StudentResources from '@/components/student-dashboard/student-resources/student-resources'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <StudentResources/>
+    </div>
+  )
+}
